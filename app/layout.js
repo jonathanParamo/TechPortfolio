d455e5339f07715ad170dffb6e56c1a5166aaa67
jonathanPramo <jhonathan-and@outlook.com>
2026-03-20 00:07:47 -0500

    fix: force dark mode, remove theme toggle

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
         {/* <ThemeProvider> */}
         {children}
